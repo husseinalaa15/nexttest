4b3f8971a9e229d2b23ce5cbdca9282b6f006c2f
Hussein Alaa <74919905+husseinalaa15@users.noreply.github.com>
2024-05-20 23:50:07 +0300

    first test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import { useEffect } from 'react';
 
+function disableReactRefresh() {
+  if (typeof window !== 'undefined') {
+    // Detect Internet Explorer using user-agent
+    const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+    if (isIE) {
+      // Disable React Refresh for Internet Explorer
+      window.$RefreshReg$ = () => {};
+      window.$RefreshSig$ = () => () => {};
+    }
+  }
+}
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    disableReactRefresh();
+  }, []);
   return <Component {...pageProps} />;
 }
---
+++ pages/downloadedge/index.js
@@ -0,0 +1,41 @@
+const DownloadEdge = ()=>{
+    return (
+        <div className="ie-layout">
+            <img src="/images/iebg.png"/>
+                <div className="downloadEdgewrapper">
+                    <div className="downloadEdgecard-parent">
+                        <div className="downloadEdgecard">
+                            <div className="downloadEdgeimg"></div>
+                            <div className="downloadEdgecontent">
+                                <div className="downloadEdgetitle">
+                                    We don’t support IE anymore
+                                    </div>
+                                <div className="downloadEdgesubtitle">
+                                    To be enable to browse our website please use edge browser, since IE is not supported any more.
+                                    </div>
+                                <div className="downloadEdgebutton">
+                                    <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a>
+                                        </div>
+                                        </div>
+                                        </div>
+                                        </div>
+                                        <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a> <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a> <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a> <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a> <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a> <a href="https://www.microsoft.com/en-us/edge/download?form=MA13IX">
+                                        Download Edge
+                                        </a>
+                </div>
+        </div>
+    )
+}
+export default DownloadEdge
